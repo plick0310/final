6bf17ddb21aa9db4f365d26e1f04fe86e8a98921
user1 <user1@DESKTOP-0ONFKAR>
2017-06-29 15:26:00 +0900

    이수정수정

--- wooltari/src/main/java/com/wooltari/study/StudyService.java
+++ wooltari/src/main/java/com/wooltari/study/StudyService.java
@@ -6,4 +6,8 @@ import java.util.List;
 public interface StudyService {
 	public List<StudyCategory> listBigCategory();
 	public List<StudyCategory> listSmallCategory(int parent);
+	
+	public List<StudyCity> listBigCity();
+	public List<StudyCity> listSmallCity(int parent);
+	
 }
