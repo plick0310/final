568d9dc2ae60d1e331a8e2af22f2a528a7889193
user1 <user1@LAPTOP-1OKPDK4C>
2017-07-06 10:21:18 +0900

    hs

---
+++ wooltari/src/main/java/com/wooltari/studyMarketBoard/StudyMarketBoardServiceImpl.java
@@ -0,0 +1,129 @@
+package com.wooltari.studyMarketBoard;
+
+import java.util.List;
+import java.util.Map;
+
+public class StudyMarketBoardServiceImpl implements StudyMarketBoardService{
+
+	@Override
+	public int insertBoard(StudyMarketBoardService dto, String pathname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<StudyMarketBoardService> listBoard(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int dataCount(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public StudyMarketBoard readBoard(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateHitCount(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public StudyMarketBoard preReadBoard(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudyMarketBoard nextReadBoard(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateBoard(StudyMarketBoardService dto, String pathname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBoard(int num, String saveFilename, String pathname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertLikeBoard(StudyMarketBoard dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int countLikeBoard(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBoardId(String userId, String root) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertReply(Reply dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Reply> listReply(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reply> listReplyAnswer(int answer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int replyDataCount(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replyCountAnswer(int answer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteReply(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertReplyLike(Reply dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Object> replyCountLike(int replyNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
