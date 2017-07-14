6c29f328939619928a9d20fe0b3c16b8126326bb
user1 <user1@Laptop>
2017-07-14 17:53:58 +0900

    gggggg

--- wooltari/src/main/java/com/wooltari/study/search/SearchController.java
+++ wooltari/src/main/java/com/wooltari/study/search/SearchController.java
@@ -1,11 +1,22 @@
 package com.wooltari.study.search;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("study.search.SearchController")
 public class SearchController {
 	@Autowired
 	private SearchService service;
-	
+
+	@RequestMapping(value="/study/search", method=RequestMethod.GET)
+	public String createdForm(Model model, HttpSession session)throws Exception {
+		
+		
+		return ".studysearch.search";
+	}
 }
