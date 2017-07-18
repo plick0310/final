22b30a3b7f435414442bb3e00f26f7a2e8467404
user1 <user1@Laptop>
2017-07-18 20:34:51 +0900

    맞춤 스터디 찾기 커밋 - ㅈㅇ

--- wooltari/src/main/java/com/wooltari/study/search/SearchService.java
+++ wooltari/src/main/java/com/wooltari/study/search/SearchService.java
@@ -1,5 +1,10 @@
 package com.wooltari.study.search;
 
-public interface SearchService {
+import java.util.List;
+import java.util.Map;
 
+
+public interface SearchService {
+	public List<StudySearch> listSearch(Map<String, Object> map);
+	public int dataCount(Map<String, Object> map);
 }
